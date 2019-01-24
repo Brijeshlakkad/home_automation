4aa90a4138a5567f2acf34b3d9483d1b03a17835
Brijeshlakkad <brijeshlakkad22@gmail.com>
2019-01-24 20:21:58 +0530

    Added login and register modules using AngularJS

---
+++ login_data.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+$session_name="Userid";
+require_once("config.php");
+require_once("user_data.php");
+function check_user($gotData){
+  $email=$gotData->user->email;
+  $password=$gotData->user->password;
+  $sql="SELECT * FROM user where email='$email' and password='$password'";
+  $result=mysqli_query($gotData->con,$sql);
+  if($result && (mysqli_num_rows($result)==1))
+  {
+    $gotData->error=false;
+    $u=getUserDataUsingEmail($gotData->con,$email);
+    if($u->error){
+      $gotData->error=true;
+      $gotData->errorMessege="Email or Password is wrong!";
+      return $gotData;
+    }
+    $gotData->user->id=$u->id;
+    $_SESSION[$gotData->session_name]=$u->id;
+    $gotData->user->location="profile.php";
+    return $gotData;
+  }
+  $gotData->error=true;
+  $gotData->errorMessege="Email or Password is wrong!";
+  return $gotData;
+}
+if(isset($_REQUEST['email']) && isset($_REQUEST['password']))
+{
+  $email=$_REQUEST['email'];
+  $password=$_REQUEST['password'];
+  $gotData=(object) null;
+  $gotData->user=(object) null;
+  $gotData->con=$con;
+  $gotData->session_name=$session_name;
+  $gotData->user->email=$email;
+  $gotData->user->password=$password;
+  $gotData=check_user($gotData);
+  $gotData->con=(object) null;
+  echo json_encode($gotData);
+}
+?>
---
+++ signup_data.php
@@ -0,0 +1,46 @@
+<?php
+require_once("config.php");
+function create_user($gotData){
+  $name=$gotData->user->name;
+  $email=$gotData->user->email;
+  $password=$gotData->user->password;
+  $address=$gotData->user->address;
+  $city=$gotData->user->city;
+  $contact=$gotData->user->contact;
+  $sql="INSERT INTO user(name,email,password,address,city,mobile) VALUES('$name','$email','$password','$address','$city','$contact')";
+  $result=mysqli_query($gotData->con,$sql);
+  if($result)
+  {
+    $gotData->error=false;
+    $gotData->errorMessage=null;
+    return $gotData;
+  }
+  $gotData->error=true;
+  $gotData->errorMessage="Try again!";
+  return $gotData;
+}
+if(isset($_REQUEST['name']) && isset($_REQUEST['email']) && isset($_REQUEST['password']) && isset($_REQUEST['address']) && isset($_REQUEST['city']) && isset($_REQUEST['contact']))
+{
+  $name=$_REQUEST['name'];
+  $email=$_REQUEST['email'];
+  $password=$_REQUEST['password'];
+  $address=$_REQUEST['address'];
+  $city=$_REQUEST['city'];
+  $contact=$_REQUEST['contact'];
+  $gotData=(object) null;
+  $gotData->con=(object) null;
+  $gotData->user=(object) null;
+  $gotData->error=false;
+  $gotData->errorMessage=null;
+  $gotData->con=$con;
+  $gotData->user->name=$name;
+  $gotData->user->email=$email;
+  $gotData->user->password=$password;
+  $gotData->user->address=$address;
+  $gotData->user->city=$city;
+  $gotData->user->contact=$contact;
+  $gotData=create_user($gotData);
+  $gotData->con=(object) null;
+  echo json_encode($gotData);
+}
+?>
