1050add181b0e2e00ea9a858777c18998feff1df
Brijeshlakkad <brijeshlakkad22@gmail.com>
2019-02-01 16:38:03 +0530

    Solved error in dealer signup page

--- js/controllers/dealer/dealer_signup_controller.js
+++ js/controllers/dealer/dealer_signup_controller.js
@@ -1,46 +1,18 @@
 myApp.controller("DealerSignupController", function($scope,$http,$window) {
   $scope.typeList=[{
-    "key":"0",
+    "key":"dealer",
     "value":"Dealer"
   },
   {
-    "key":"1",
+    "key":"distributor",
     "value":"Distributor"
   }];
   $scope.s_type=$scope.typeList[0];
-	$scope.contactStyle = {
-		"border-bottom-width":"1.45px"
-  };
-  $scope.analyzeContact = function(value) {
-    $http({
-      method : "POST",
-      url : "check_data_exists.php",
-      data: "contact="+value,
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-    }).then(function mySuccess(response) {
-      flag = response.data;
-      // we should be using flag in only this block so logic in following
-      if(flag.error)
-      {
-        $scope.showContactStatus=flag.errorMessage;
-        $scope.contactStyle["border-bottom-color"] = "red";
-      }
-      else
-      {
-        $scope.showContactStatus="";
-        $scope.contactStyle["border-bottom-color"] = "green";
-      }
-    }, function myError(response) {
-      $scope.showContactStatus="";
-      $scope.contactStyle["border-bottom-color"] = "red";
-    });
-  };
-
   $scope.signup_status=function(){
     $http({
 			method : "POST",
 			url : "dealer/signup_data.php",
-			data: "email="+$scope.s_email+"&password="+$scope.s_password+"&name="+$scope.s_name+"&address="+$scope.s_address+"&city="+$scope.s_city+"&contact="+$scope.s_contact+"&type="+$scope.s_type,
+			data: "email="+$scope.s_email+"&password="+$scope.s_password+"&name="+$scope.s_name+"&address="+$scope.s_address+"&city="+$scope.s_city+"&contact="+$scope.s_contact+"&type="+$scope.s_type.key,
 			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
 		}).then(function mySuccess(response) {
 			flag = response.data;
@@ -58,7 +30,9 @@ myApp.controller("DealerSignupController", function($scope,$http,$window) {
 				$scope.s_status_0=true;
 			}
 		}, function myError(response) {
-
+      $scope.signupStatus="";
+      $scope.s_status_1=false;
+      $scope.s_status_0=true;
 		});
   };
 });
